fe1be947628f594130cefd2ee339bb8b8539bf39
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-03-27 16:02:09 +0100

    changes with spacing of the functions

--- catchment/models.py
+++ catchment/models.py
@@ -9,6 +9,7 @@ time across all sites.
 
 import pandas as pd
 
+
 def read_variable_from_csv(filename):
     """Reads a named variable from a CSV file, and returns a
     pandas dataframe containing that variable. The CSV file must contain
@@ -21,8 +22,8 @@ def read_variable_from_csv(filename):
     """
     dataset = pd.read_csv(filename, usecols=['Date', 'Site', 'Rainfall (mm)'])
 
-    dataset = dataset.rename({'Date':'OldDate'}, axis='columns')
-    dataset['Date'] = [pd.to_datetime(x,dayfirst=True) for x in dataset['OldDate']]
+    dataset = dataset.rename({'Date': 'OldDate'}, axis='columns')
+    dataset['Date'] = [pd.to_datetime(x, dayfirst=True) for x in dataset['OldDate']]
     dataset = dataset.drop('OldDate', axis='columns')
 
     newdataset = pd.DataFrame(index=dataset['Date'].unique())
@@ -34,10 +35,12 @@ def read_variable_from_csv(filename):
 
     return newdataset
 
+
 def daily_total(data):
     """Calculate the daily total of a 2d data array.
     Index must be np.datetime64 compatible format."""
     return data.groupby(data.index.date).sum()
+
 
 def daily_mean(data):
     """Calculate the daily mean of a 2D data array.
@@ -55,4 +58,3 @@ def daily_min(data):
     """Calculate the daily min of a 2d data array.
     Index must be np.datetime64 compatible format."""
     return data.groupby(data.index.date).min()
-
