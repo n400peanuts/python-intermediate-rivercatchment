f9b2d2fa8e79113f5ed059a1eebfbbf0b4688681
Eva Viviani <evaviviani@c-001-001-013.client.esciencecenter.eduvpn.nl>
2023-03-28 15:03:30 +0100

    added tests for min and max

--- tests/test_models.py
+++ tests/test_models.py
@@ -49,3 +49,69 @@ def test_daily_mean_integers():
 
     # Need to use Pandas testing functions to compare arrays
     pdt.assert_frame_equal(daily_mean(test_input), test_result)
+
+
+def test_daily_min():
+    """Test that daily min works as expected"""
+    from catchment.models import daily_min
+
+    test_input = pd.DataFrame(
+        data=[[1, 2],
+              [7, 4],
+              [9, 6]],
+        index=[pd.to_datetime('2000-01-01 01:00'),
+               pd.to_datetime('2000-01-01 02:00'),
+               pd.to_datetime('2000-01-01 03:00')],
+        columns=['A', 'B']
+    )
+    test_result = pd.DataFrame(
+                     data=[[1, 2]],
+                     index=[datetime.date(2000, 1, 1)],
+                     columns=['A', 'B']
+    )
+
+    pdt.assert_frame_equal(daily_min(test_input), test_result)
+
+def test_daily_max():
+    """Test that daily max works as expected"""
+    from catchment.models import daily_max
+
+    test_input = pd.DataFrame(
+        data=[[1, 2],
+              [7, 4],
+              [9, 6]],
+        index=[pd.to_datetime('2000-01-01 01:00'),
+               pd.to_datetime('2000-01-01 02:00'),
+               pd.to_datetime('2000-01-01 03:00')],
+        columns=['A', 'B']
+    )
+    test_result = pd.DataFrame(
+                     data=[[9, 6]],
+                     index=[datetime.date(2000, 1, 1)],
+                     columns=['A', 'B']
+    )
+
+    pdt.assert_frame_equal(daily_max(test_input), test_result)
+
+def test_daily_max_alternative():
+    """Test that daily max works as expected"""
+    from catchment.models import daily_max
+
+    test_input = pd.DataFrame(
+        data=[[1, 2],
+              [7, 4],
+              [9, 6]],
+        index=[pd.to_datetime('2000-01-01 01:00'),
+               pd.to_datetime('2000-01-01 02:00'),
+               pd.to_datetime('2000-01-01 03:00')],
+        columns=['A', 'B']
+    )
+    test_result_max = []
+    for i in range(len(test_input.columns)):
+        test_result_max.append(max(test_input.iloc[:, i]))
+    test_result = pd.DataFrame(
+        data=[test_result_max],
+        index=[datetime.date(2000, 1, 1)],
+        columns=['A', 'B']
+    )
+    pdt.assert_frame_equal(daily_max(test_input), test_result)
