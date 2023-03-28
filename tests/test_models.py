e47e564e1ae0b3d5e71e2e63c83864603f51a7cf
Eva Viviani <evaviviani@c-001-001-013.client.esciencecenter.eduvpn.nl>
2023-03-28 15:46:43 +0100

    added pytest decorator to run multiple datasets

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import pandas.testing as pdt
 import datetime
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -115,3 +116,84 @@ def test_daily_max_alternative():
         columns=['A', 'B']
     )
     pdt.assert_frame_equal(daily_max(test_input), test_result)
+
+@pytest.mark.parametrize(
+    "test_data, test_index, test_columns, expected_data, expected_index, expected_columns",
+    [
+        (
+            [[0.0, 0.0], [0.0, 0.0], [0.0, 0.0]],
+            [pd.to_datetime('2000-01-01 01:00'),
+             pd.to_datetime('2000-01-01 02:00'),
+             pd.to_datetime('2000-01-01 03:00')],
+            ['A', 'B'],
+            [[0.0, 0.0]],
+            [datetime.date(2000, 1, 1)],
+            ['A', 'B']
+        ),
+        (
+            [[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]],
+            [pd.to_datetime('2000-01-01 01:00'),
+             pd.to_datetime('2000-01-01 02:00'),
+             pd.to_datetime('2000-01-01 03:00')],
+            ['A', 'B'],
+            [[3.0, 4.0]],
+            [datetime.date(2000, 1, 1)],
+            ['A', 'B']
+        )
+    ]
+)
+
+
+def test_daily_mean(test_data, test_index, test_columns,
+                    expected_data, expected_index, expected_columns):
+    """Test whether mean function works with zeros and positive integers"""
+    from catchment.models import daily_mean
+    pdt.assert_frame_equal(
+        daily_mean(pd.DataFrame(
+            data=test_data,
+            index=test_index,
+            columns=test_columns
+            )
+        ),
+        pd.DataFrame(
+            data=expected_data,
+            index=expected_index,
+            columns=expected_columns
+        )
+    )
+
+@pytest.mark.parametrize(
+    "test_data_max, test_index_max, test_columns_max, expected_data_max, expected_index_max, expected_columns_max",
+    [
+        (
+            [[1.0, 2.0], [7.0, 4.0], [9.0, 6.0]],
+            [pd.to_datetime('2000-01-01 01:00'),
+             pd.to_datetime('2000-01-01 02:00'),
+             pd.to_datetime('2000-01-01 03:00')],
+            ['A', 'B'],
+            [[9.0, 6.0]],
+            [datetime.date(2000, 1, 1)],
+            ['A', 'B']
+        )
+    ]
+)
+
+def test_daily_max(test_data_max, test_index_max, test_columns_max,
+                    expected_data_max, expected_index_max, expected_columns_max):
+    """Test whether mean function works with zeros and positive integers"""
+    from catchment.models import daily_max
+    pdt.assert_frame_equal(
+        daily_max(pd.DataFrame(
+            data=test_data_max,
+            index=test_index_max,
+            columns=test_columns_max
+            )
+        ),
+        pd.DataFrame(
+            data=expected_data_max,
+            index=expected_index_max,
+            columns=expected_columns_max
+        )
+    )
+
+
