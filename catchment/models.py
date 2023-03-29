56ac69a83b75455ea6a50505d53527f02af34e20
Eva Viviani <evaviviani@Evas-MacBook-Pro.local>
2023-03-29 11:18:58 +0100

    added test

--- catchment/models.py
+++ catchment/models.py
@@ -8,6 +8,13 @@ time across all sites.
 """
 
 import pandas as pd
+import numpy as np
+
+
+def data_normalise(data):
+    """Normalise data"""
+    max_ = np.array(np.max(data, axis=0))
+    return data / max_[np.newaxis, :]
 
 def read_variable_from_csv(filename):
     """Reads a named variable from a CSV file, and returns a
